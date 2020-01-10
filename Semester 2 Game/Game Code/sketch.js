abc65dbea47a8ebb26db2c811e0bb3a8d5f91199
SethReis <795091@seq.org>
2020-01-10 15:01:32 -0800

    Initial Commit

---
+++ Semester 2 Game/Game Code/sketch.js
@@ -0,0 +1,14 @@
+//  Seth Reis, Joey Jacob
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(windowWidth*2, windowHeight*2);
+  cnv.position(0, 0);
+  background(0);
+}
+
+//  The draw function is called @ 30 fps
+function draw(){
+  background(0);
+}
