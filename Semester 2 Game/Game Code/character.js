c86c7f17de05903d6b4da73a3dd2e23a5e304eee
SethReis <795091@seq.org>
2020-01-14 14:55:41 -0800

    Jumping has begun working

---
+++ Semester 2 Game/Game Code/character.js
@@ -0,0 +1,23 @@
+class Character{
+  constructor(x, y, dx, dy, idNum){
+
+  }
+
+  run(){
+    this.checkEdges();
+    this.update();
+    this.render();
+  }
+
+  checkEdges(){
+
+  }
+
+  update(){
+
+  }
+
+  render(){
+
+  }
+}
