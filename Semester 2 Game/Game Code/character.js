9e87c580e43bf8f7b743bfdda133503f80828cad
SethReis <795091@seq.org>
2020-01-14 15:29:59 -0800

    Moved Stuff to Character Class

--- Semester 2 Game/Game Code/character.js
+++ Semester 2 Game/Game Code/character.js
@@ -1,23 +1,60 @@
 class Character{
-  constructor(x, y, dx, dy, idNum){
-
+  constructor(x, y){
+    this.loc = createVector(x, y)
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.xChange = 0;
+    this.yChange = 0;
+    this.moving = false;
   }
 
   run(){
     this.checkEdges();
+    this.checkKeys();
     this.update();
     this.render();
   }
 
   checkEdges(){
+    if(this.loc.y > 3*height/4){
+      this.xChange = 0;
+      this.yChange = 0;
+      this.moving = false;
+      this.acc = createVector(0, 0);
+      this.vel = createVector(0, 0);
+      this.loc.y = 3*height/4;
+    }
+  }
 
+  checkKeys(){
+    if(keyIsDown(RIGHT_ARROW)){
+      this.xChange++;
+    }
+    if(keyIsDown(LEFT_ARROW)){
+      this.xChange--;
+    }
+    if(keyIsDown(UP_ARROW)){
+      this.yChange--;
+    }
   }
 
   update(){
-
+    if(((this.xChange != 0 || this.yChange != 0)) && keyIsPressed === false && this.moving === false){
+      this.acc = createVector(0, 0.5);
+      this.vel = createVector(this.xChange/2, this.yChange/2);
+      this.moving = true;
+    }
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
   }
 
   render(){
-
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    if (keyIsPressed === true && this.moving === false){
+      rect(this.loc.x-40, this.loc.y-20, 80, 20);
+    } else {
+      rect(this.loc.x-20, this.loc.y-40, 40, 40);
+    }
   }
 }
--- Semester 2 Game/Game Code/sketch.js
+++ Semester 2 Game/Game Code/sketch.js
@@ -2,54 +2,18 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var x = 0;
-var y = 0;
-var loc;
-var vel;
-var acc;
-var moving = false;
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.position(0, 0);
   background(0);
-  loc = createVector(width/2-20, height/2-40)
-  vel = createVector(0, 0);
-  acc = createVector(0, 0);
+  char = new Character(width/2, 3*height/4);
 }
 
 //  The draw function is called @ 30 fps
 function draw(){
   background(0);
   stroke(0, 0, 255);
-  line(0, height/2, width, height/2);
+  line(0, 3*height/4, width, 3*height/4);
   line(width/2, 0, width/2, height);
-  if(((x != 0 || y != 0)) && keyIsPressed === false && moving === false){
-    acc = createVector(0, 0.5);
-    vel = createVector(x/2, y/2);
-    moving = true;
-  }
-  vel.add(acc);
-  loc.add(vel);
-
-  if(keyIsDown(RIGHT_ARROW)){
-    x++;
-  }
-  if(keyIsDown(LEFT_ARROW)){
-    x--;
-  }
-  if(keyIsDown(UP_ARROW)){
-    y--;
-  }
-  fill(255, 0, 0);
-  stroke(255, 0, 0);
-  rect(loc.x, loc.y, 40, 40);
-
-  if(loc.y > height/2-40){
-    x = 0;
-    y = 0;
-    moving = false;
-    acc = createVector(0, 0);
-    vel = createVector(0, 0);
-    loc.y = height/2-40;
-  }
+  char.run();
 }
