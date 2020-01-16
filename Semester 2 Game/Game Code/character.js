8a498eb46e6749c8eeae6c2c0c88663986373c0d
SethReis <795091@seq.org>
2020-01-16 15:12:19 -0800

    collisions checks redid

--- Semester 2 Game/Game Code/character.js
+++ Semester 2 Game/Game Code/character.js
@@ -19,41 +19,44 @@ class Character{
   }
 
   checkEdges(){
-    //check if land on top
-    if(this.loc.x+20 > plat.x && this.loc.x-20 < plat.x + plat.w
-       && this.loc.y > plat.y && this.loc.y < plat.y + plat.h && this.vel.y >= 0){
-      this.sideState = 1;
-    }
-    //check if hit right
-    if(this.loc.x-20 > plat.x + plat.w-20 && this.loc.x-20 < plat.x + plat.w
-       && this.loc.y > plat.y && this.loc.y-40 < plat.y + plat.h && this.vel.x <= 0){
-      this.sideState = 2;
-    }
-    //check if hit left
-    if(this.loc.x+20 > plat.x && this.loc.x+20 < plat.x + 20
-       && this.loc.y > plat.y && this.loc.y-40 < plat.y + plat.h && this.vel.x >= 0){
-      this.sideState = 2;
-    }
-    //check if hit bottom
-    if(this.loc.x+20 > plat.x && this.loc.x-20 < plat.x + plat.w
-       && this.loc.y-40 > plat.y + plat.h-20 && this.loc.y-40 < plat.y + plat.h && this.vel.y <= 0){
-      this.sideState = 3;
-    }
+    for (var i = 0; i < platforms[gameScreen].length; i++){
+      let plat = platforms[gameScreen][i];
+      //check if land on top
+      if(this.loc.x+20 > plat.x && this.loc.x-20 < plat.x + plat.w
+         && this.loc.y > plat.y && this.loc.y < plat.y + plat.h && this.vel.y >= 0){
+        this.sideState = 1;
+      }
+      //check if hit right
+      if(this.loc.x-20 > plat.x + plat.w-20 && this.loc.x-20 < plat.x + plat.w
+         && this.loc.y > plat.y && this.loc.y-40 < plat.y + plat.h && this.vel.x <= 0){
+        this.sideState = 2;
+      }
+      //check if hit left
+      if(this.loc.x+20 > plat.x && this.loc.x+20 < plat.x + 20
+         && this.loc.y > plat.y && this.loc.y-40 < plat.y + plat.h && this.vel.x >= 0){
+        this.sideState = 2;
+      }
+      //check if hit bottom
+      if(this.loc.x+20 > plat.x && this.loc.x-20 < plat.x + plat.w
+         && this.loc.y-40 > plat.y + plat.h-20 && this.loc.y-40 < plat.y + plat.h && this.vel.y <= 0){
+        this.sideState = 3;
+      }
 
-    if(this.sideState === 1){
-      this.xChange = 0;
-      this.yChange = 0;
-      this.moving = false;
-      this.acc = createVector(0, 0);
-      this.vel = createVector(0, 0);
-      this.loc.y = plat.y;
-    } else if(this.sideState === 2){
-      this.vel.x = -this.vel.x;
-    } else if(this.sideState === 3){
-      this.vel.y = -this.vel.y;
-    }
+      if(this.sideState === 1){
+        this.xChange = 0;
+        this.yChange = 0;
+        this.moving = false;
+        this.acc = createVector(0, 0);
+        this.vel = createVector(0, 0);
+        this.loc.y = plat.y;
+      } else if(this.sideState === 2){
+        this.vel.x = -this.vel.x;
+      } else if(this.sideState === 3){
+        this.vel.y = -this.vel.y;
+      }
 
-    this.sideState = 0;
+      this.sideState = 0;
+    }
   }
 
   checkKeys(){
